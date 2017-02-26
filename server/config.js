0caf06651bb343afb3af21ef1916c5c0ce3fc691
Hieu <holechihieu@gmail.com>
2017-02-26 23:16:22 +0700

    Add mongo account

--- server/config.js
+++ server/config.js
@@ -3,4 +3,8 @@ const config = {
   port: process.env.PORT || 3001,
 };
 
+if (process.env.NODE_ENV === 'production') {
+  config.mongoURL = 'mongodb://hieuhlc:123123@ds161169.mlab.com:61169/heroku_x97bsbq6'
+}
+
 export default config;
